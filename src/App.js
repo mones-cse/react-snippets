8d6477df3fc8f5b3021f5f03da90ae4cd7f629ca
mones <mones.cse@gmail.com>
2021-07-12 22:02:51 +0600

    useMemo example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import "./App.scss";
+import { useState, useMemo, useEffect } from "react";
 
 function App() {
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+
+    const themeStyle = useMemo(() => {
+        return {
+            backgroundColor: dark ? "black" : "white",
+            color: dark ? "white" : "black",
+        };
+    }, [dark]);
+    useEffect(() => {
+        console.log("Theme Change");
+    }, [themeStyle]);
+
+    const slowFunction = number => {
+        for (let i = 0; i < 1000000000; i++) {}
+        return number * 2;
+    };
+    const doubleNumber = useMemo(() => {
+        return slowFunction(number);
+    }, [number]);
+    // if number same we don't recalculate slowFunction instead we return cache value
+
     return (
-        <div className="App">
-            <div className={"bg-secondary min-vh-100"}>
-                <h1>Hello</h1>
-            </div>
+        <div className="App container">
+            <input
+                type="text"
+                value={number}
+                onChange={e => setNumber(e.target.value)}
+            />
+            <br />
+            <button
+                className={"btn btn-primary"}
+                onClick={() => setDark(!dark)}
+            >
+                Toggle
+            </button>
+            <div style={themeStyle}>{doubleNumber}</div>
         </div>
     );
 }
