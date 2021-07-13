175352d6abdb98ed8a872ccddb4cf24b5c084109
mones <mones.cse@gmail.com>
2021-07-13 12:28:36 +0600

    usecallback example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import "./App.scss";
+import {useEffect, useState, useCallback} from "react";
 
+function List({getNumber}){
+    const [items,setItems] =useState([]);
+
+    useEffect(()=>{
+        setItems(getNumber());
+        console.log('updating items');
+    },[getNumber]);
+
+    return items.map(item => <div key={item}>{item}</div>);
+
+};
 function App() {
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+
+    const themeStyle = {
+        backgroundColor: dark ? "black" : "white",
+        color: dark ? "white" : "black",
+    };
+    // following function re created everytime App function rerender
+    // so we need to useCallback to make sure only execute this function if number changes
+
+    const getNumber = useCallback(()=>{
+        return [number,number+1,number+2];
+    },[number]);
+
+    // difference between useMemo and useCallback
+    // useMemo takes a function and return `returned value` of that function
+    // other hand useCallback takes a function and return the function
+
+
     return (
-        <div className="App">
-            <div className={"bg-secondary min-vh-100"}>
-                <h1>Hello</h1>
+        <div className="container">
+            <div style={themeStyle}>
+                <input type="number" value={number} onChange={e=>setNumber(parseInt(e.target.value))}/>
+                <br/>
+                <button className={"btn btn-primary"} onClick={()=>setDark(!dark)}>Toggle</button>
+                <List getNumber={getNumber}/>
             </div>
         </div>
     );
