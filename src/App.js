7f7e3b9256ee6aa1cd12bab538f9cefd51d74e77
mones <mones.cse@gmail.com>
2021-07-13 15:38:44 +0600

    counter app using state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.scss";
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+
+    const increment = ()=>{
+        setCount(prevCount => prevCount+1);
+    };
+    const decrement = ()=>{
+        setCount(prevCount => prevCount-1);
+    };
     return (
-        <div className="App">
-            <div className={"bg-secondary min-vh-100"}>
-                <h1>Hello</h1>
-            </div>
+        <div className="container bg-light min-vh-100 text-center">
+           <button className={"btn btn-lg btn-info m-5"} onClick={increment}>+</button>
+            <span className={"p-5"}>{count}</span>
+           <button className={"btn btn-lg btn-info m-5"} onClick={decrement}>-</button>
+
         </div>
     );
 }
