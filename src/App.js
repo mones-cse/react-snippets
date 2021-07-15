d49222312207d566688c5d6a34be91b8804ae6e0
mones <mones.cse@gmail.com>
2021-07-16 02:06:35 +0600

    basic react portal example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import ReactDOM from "react-dom";
+
 function App() {
-    return <div className="Container">
-        <div className={"bg-light col-8 m-auto min-vh-100 text-center"}>
-            Hello
+    const child = <div className={"text-center bg-info"}>Every body</div>;
+    const container = document.getElementById("modal-root");
+
+    return (
+        <div className="Container">
+            <div className={"bg-light col-8 m-auto h-25 text-center"}>
+                Hello
+            </div>
+            {ReactDOM.createPortal(child, container)}
         </div>
-    </div>;
+    );
 }
 
 export default App;
