0ee2a977efba1b585a4410b5b9f81666759503a8
mones <mones.cse@gmail.com>
2021-07-19 11:55:16 +0600

    initial stage with state

--- src/BookmarkComponent.js
+++ src/BookmarkComponent.js
@@ -1,21 +1,29 @@
-import React, {useState} from 'react';
-import HOC from './HOC';
-const BookMarkComponent = (props) => {
+import React, { useState } from "react";
+
+const BookMarkComponent = props => {
     // following code handled by hoc
-    // const [bookmarks, setBookmarks] = useState(0);
-    // const increaseBookmark = ()=>{
-    //     setBookmarks(prevLikes=>prevLikes+1);
-    // };
-    // const decreaseBookmark = ()=>{
-    //     setBookmarks(prevLikes=>prevLikes+1);
-    // };
+    const [counts, setCounts] = useState(0);
+    const increaseCount = () => {
+        setCounts(prevLikes => prevLikes + 1);
+    };
+
+    const decreaseCount = () => {
+        setCounts(prevLikes => prevLikes - 1);
+    };
     return (
         <div className={"col-2 p-5 card"}>
-            <button className={"btn btn-lg btn-primary"} onClick={props.increaseCount}>BookMark</button>
-            <p className={"m-4"}>Total BookMark {props.counts}</p>
-            <button className={"btn btn-lg btn-danger"} onClick={props.decreaseCount}>Remove BookMark</button>
+            <button
+                className={"btn btn-lg btn-primary"}
+                onClick={increaseCount}
+            >
+                BookMark
+            </button>
+            <p className={"m-4"}>Total BookMark {counts}</p>
+            <button className={"btn btn-lg btn-danger"} onClick={decreaseCount}>
+                Remove BookMark
+            </button>
         </div>
     );
 };
 
-export default HOC(BookMarkComponent);
+export default BookMarkComponent;
--- src/LikeComponent.js
+++ src/LikeComponent.js
@@ -1,24 +1,31 @@
-import React, {useState} from "react";
-import HOC from "./HOC";
-const LikeComponent = (props) => {
+import React, { useState } from "react";
+
+const LikeComponent = props => {
     // following code handled by hoc
-    // const [likes, setLikes] = useState(0);
-    // const increaseLike = ()=>{
-    //     setLikes(prevLikes=>prevLikes+1);
-    // };
-    //
-    // const decreaseLike = ()=>{
-    //     setLikes(prevLikes=>prevLikes-1);
-    // };
+    const [counts, setCounts] = useState(0);
+    const increaseCount = () => {
+        setCounts(prevLikes => prevLikes + 1);
+    };
+
+    const decreaseCount = () => {
+        setCounts(prevLikes => prevLikes - 1);
+    };
     return (
         <div className={"col-2 p-5 card text-center"}>
-            <button className={"btn btn-lg btn-primary"} onClick={props.increaseCount}>Like</button>
-            <p className={"m-4"}>Total like {props.counts}</p>
-            <button className={"btn btn-lg btn-danger"} onClick={props.decreaseCount}>Dislike</button>
+            <button
+                className={"btn btn-lg btn-primary"}
+                onClick={increaseCount}
+            >
+                Like
+            </button>
+            <p className={"m-4"}>Total like {counts}</p>
+            <button className={"btn btn-lg btn-danger"} onClick={decreaseCount}>
+                Dislike
+            </button>
 
             <p>{props.test}</p>
         </div>
     );
 };
 
-export default HOC(LikeComponent,1);
+export default LikeComponent;
